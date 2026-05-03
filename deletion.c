64a1b757b54d4182aac359acb9a759b4101fc6d7
Adnan_Hussain <itadnanhussain@gmail.com>
2026-05-04 00:22:37 +0600

    Added new operation

---
+++ deletion.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+int main()
+{
+
+    int n;
+    printf("Enter Array Size: ");
+    scanf("%d",&n);
+
+    int arr[n];
+
+    for(int i=0;i<n;i++)
+    {
+        printf("Enter %d index element: ",i+1);
+        scanf("%d",&arr[i]);
+    }
+
+    int idx;
+    printf("Enter Which index you want to delete: ");
+    scanf("%d",&idx);
+
+    for(int i=idx;i<n;i++)
+    {
+        arr[i] = arr[i+1];
+    }
+
+    printf("Now Updated Array: ");
+
+    for(int i=0;i<n-1;i++)
+    {
+        printf("%d ",arr[i]);
+    }
+
+    printf("\n");
+
+
+
+
+
+    return 0;
+}
